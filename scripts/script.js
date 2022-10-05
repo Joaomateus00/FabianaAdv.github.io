53e158457cd7041a689108ee122a9f2fd010f7fb
Joaomateus00 <joaoommateus@gmail.com>
2022-10-05 11:42:17 -0300

    commit script

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+function background(){
+    let back = document.getElementById('martelo')
+    back.style.display = 'flex';
+}
+function removeBack(){
+    let back = document.getElementById('martelo');
+    back.style.display = 'none';
+}
+function backgroundFamilia(){
+    let back = document.getElementById('familia')
+    back.style.display = 'flex';
+}
+function removeBackFamilia(){
+    let back = document.getElementById('familia');
+    back.style.display = 'none';
+}
+function backgroundCasa(){
+    let back = document.getElementById('casa')
+    back.style.display = 'flex';
+}
+function removeBackCasa(){
+    let back = document.getElementById('casa');
+    back.style.display = 'none';
+}
+function backgroundNoivos(){
+    let back = document.getElementById('noivos')
+    back.style.display = 'flex';
+}
+function removeBackNoivos(){
+    let back = document.getElementById('noivos');
+    back.style.display = 'none';
+}
